7212d34f8ad8595a82db8a050e03698beff0e77f
Joe Sak <joe@joesak.com>
2022-07-02 13:09:39 -0500

    Upgrading, rewriting

--- src/Nav.js
+++ src/Nav.js
@@ -3,11 +3,7 @@ import NavLink from './NavLink'
 export default function Nav() {
   return (
     <nav className="flex">
-      <div className="flex">
-        <NavLink href="/joseph-sak.pdf" target="_blank">Resume</NavLink>
-        <NavLink href="//github.com/joemsak">Github</NavLink>
-        <NavLink mailTo="joe.sak@hey.com">Email</NavLink>
-      </div>
+      <NavLink mailTo="joe.sak@hey.com">Email</NavLink>
     </nav>
   )
 }
