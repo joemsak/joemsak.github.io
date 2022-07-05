6825348727ddc1ec830235db6ac7c4e1bf909be3
Joe Sak <joe@joesak.com>
2022-07-05 11:14:11 -0500

    Add 'about me'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import Main from './Main'
+import About from './About'
+import Greeting from './Greeting'
+import Pitch from './Pitch'
+import Booking from './Booking'
 
 export default function App() {
   return (
-    <>
-      <Main />
-    </>
-  );
+    <main className="text-gray-700">
+      <Greeting />
+      <Pitch />
+
+      <div className="mt-4">
+        <About />
+      </div>
+
+      <Booking />
+    </main>
+  )
 }
