811900e0b4dfd1813b2fef57995c8975d7d5c29f
Joe Sak <joe@joesak.com>
2024-03-16 09:24:11 -0600

    Improve greeting

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
-import weekDayName from './utils/week-day-name'
+import weekDayName from "./utils/week-day-name";
+import weekDayEmotion from "./utils/week-day-emotion";
 
-export { weekDayName }
+export { weekDayName, weekDayEmotion };
