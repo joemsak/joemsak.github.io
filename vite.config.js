f79b86b2f963a64d5c2e888874122c38f7ad9b64
Joe Sak <joe@joesak.com>
2024-08-05 14:12:52 -0600

    More refactor, privacy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import compress from "vite-plugin-compress";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root: "src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./src/index.html"),
+        legal: resolve(__dirname, "./src/privacy/index.html"),
+      },
+    },
+  },
+  plugins: [compress, react],
 });
