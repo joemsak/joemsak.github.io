081880d456441090a80bf744e724e6a1e208a64b
Joe Sak <joe@joesak.com>
2024-08-05 14:03:39 -0600

    Refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ["./src/**/*"],
 
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
@@ -16,4 +16,4 @@ module.exports = {
   },
 
   plugins: [],
-}
+};
